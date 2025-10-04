ed84655a05595dac0a66a381205bc8e0dcf028e3
teligramboturl <teligramboturl@gmail.com>
2025-10-05 04:39:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,20 +9,3 @@ class Config(object):
     ADMIN_ID = [6677821706]
     # MongoDB database ka URL
     DB_URL = "mongodb+srv://sindhavravi11:%24Sind1234567890@cluster0.bwlgbx3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    # Database ka naam
-    DB_NAME = "MY_BOT_DB"
-    # Text log channel ki ID
-    TXT_LOG = "-1002755790239"
-    # Authentication log channel ki ID
-    AUTH_LOG = "-1002755790239"
-    # Hit log channel ki ID
-    HIT_LOG = "-1002755790239"
-    # DRM dump channel ki ID
-    DRM_DUMP = "-1002755790239"
-    CH_URL = "https://t.me/neyyeneejeywyneme"
-    # Bot ke owner ka Telegram link
-    OWNER = "https://t.me/paidcoursefree_video"
-    # Thumbnail image ka URL
-    THUMB_URL = "https://telegra.ph/file/example-thumb-image.jpg" #Replace by with your Thumb URL
-    PREMIUM_LOGS = "-1002755790239"
-    
