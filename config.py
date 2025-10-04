7f4c6d226b9e2e6bfa1368bcf9c0fb9f9d7cb67e
teligramboturl <teligramboturl@gmail.com>
2025-10-05 04:23:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
     BOT_TOKEN = "8307502375:AAH0_nZ784i-0Hdg4d75TlpIveBMbV7ynqE"
     # Telegram API ki ID
     API_ID = "23673651"
-    API_HASH = f032bfa12ee46e1283f6fb23cfca5c6b"
+    API_HASH = "f032bfa12ee46e1283f6fb23cfca5c6b"
     ADMIN_ID = [6677821706]
     # MongoDB database ka URL
     DB_URL = "mongodb+srv://sindhavravi11:%24Sind1234567890@cluster0.bwlgbx3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
